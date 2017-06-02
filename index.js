f710b0d1a1cd03ec58c959623b2a596bbcc957d4
Cameron Jenner <cjenner37@gmail.com>
2017-06-02 12:38:47 -0600

    fixing stuff

--- index.js
+++ index.js
@@ -9,10 +9,16 @@ var body = document.getElementsByTagName('body')[0];
 
 function myFunction() {
 	let time = new Date();
-	let hour = time.getHours();
+	let hours = time.getHours();
 	let minutes = time.getMinutes();
 	let seconds = time.getSeconds();
-	let color = ( "#" + hour + "" + minutes + "" + seconds);
+	if (hours < 10) {
+		hours = "0" + hours;}
+	if (minutes < 10) {
+		minutes = "0" + minutes;}
+	if (seconds < 10) {
+		seconds = "0" + seconds;}
+	let color = ( "#" + hours + "" + minutes + "" + seconds);
 	body.style.backgroundColor = color;
 	clock.innerText = color;
 };
